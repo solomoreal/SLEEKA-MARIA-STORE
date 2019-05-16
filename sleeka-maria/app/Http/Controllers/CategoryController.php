65c782869045d4d4781ad82d2f7b71b696e3af85
solomoreal <solomoreal@yahoo.com>
2019-05-16 12:17:47 +0100

    test data

--- sleeka-maria/app/Http/Controllers/CategoryController.php
+++ sleeka-maria/app/Http/Controllers/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::latest()->get();
         return $categories;
     }
 
