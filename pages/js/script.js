ba09a51900f745f21fef1d510a205a1b2544a485
codxxx <calebCloud94@gmail.com>
2019-05-23 16:47:00 +0100

    feat(User Sign Up): Registration form
    --ensure user can register
    [Delivers #166214378]

--- pages/js/script.js
+++ pages/js/script.js
@@ -1,17 +1,33 @@
-// var modal = document.querySelector(".modalx");
-// var trigger = document.querySelector(".trigger");
-// var closeButton = document.querySelector(".close-button");
+var modal = document.querySelector(".modalx");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
 
-// function toggleModal() {
-//   modal.classList.toggle("show-modal");
-// }
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
 
-// function windowOnClick(event) {
-//   if (event.target === modal) {
-//     toggleModal();
-//   }
-// }
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
 
-// trigger.addEventListener("click", toggleModal);
-// closeButton.addEventListener("click", toggleModal);
-// window.addEventListener("click", windowOnClick);
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
